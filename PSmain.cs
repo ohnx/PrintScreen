a49678128d73dfb358d8d318046793d2567a220f
Mason X <masonx.01@gmail.com>
2015-09-12 22:52:37 -0400

    code

---
+++ PSmain.cs
@@ -0,0 +1,88 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace PS
+{
+    static class PSmain
+    {
+        static NotifyIcon nIco;
+        static string savePath, currPath;
+        static System.Random rnd;
+        static Rectangle screenCombined;
+        static Graphics gfx = Graphics.FromHwnd(System.IntPtr.Zero);
+        static Icon b, w;
+
+        static void Main(string[] args)
+        {
+            w = new Icon(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("PS.cam-w.ico"));
+            b = new Icon(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("PS.cam-b.ico"));
+            rnd = new System.Random();
+            savePath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyPictures) + "\\Screenshots\\Screenshot ";
+            nIco = new NotifyIcon();
+            nIco.Icon = w;
+            nIco.Visible = true;
+            initContextMenu();
+            nIco.Click += PrintScreen;
+            //nIco.ShowBalloonTip(100, "PrintScreen launched!", "Right-click the icon in the system tray to configure it.", ToolTipIcon.Info);
+            Application.Run();
+        }
+        private static void PrintScreen(object a, object bee)
+        {
+            nIco.Icon = b;
+            try
+            {
+                screenCombined = new Rectangle(int.MaxValue, int.MaxValue, int.MinValue, int.MinValue);
+                foreach (Screen screen in Screen.AllScreens)
+                {
+                    screenCombined = Rectangle.Union(screenCombined, screen.Bounds);
+                }
+
+                Bitmap printscreen = new Bitmap(screenCombined.Width, screenCombined.Height);
+                Graphics graphics = Graphics.FromImage(printscreen as Image);
+                graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+                currPath = savePath + System.DateTime.Now.ToString("MM-dd-yy HH.mm.ss ") + rnd.Next(10, 99) + ".png";
+
+                printscreen.Save(currPath, ImageFormat.Png);
+            }
+            catch (System.Exception e)
+            {
+                MessageBox.Show("Error saving file: " + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            nIco.Icon = w;
+        }
+        private static void help(object a, object bee)
+        {
+            MessageBox.Show("PrintScreen is a simple application that captures your screen to a file in your Pictures folder.\nTo use it, simply click/tap on the icon in the system tray.\nTo quit it, just right-click and then hit \"Quit\"", "About PrintScreen", MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+        private static void quit(object a, object bee)
+        {
+            nIco.Visible = false;
+            Application.Exit();
+        }
+        private static void initContextMenu()
+        {
+            MenuItem PSNameItem = new MenuItem("PrintScreen v1.0");
+            MenuItem HelpItem = new MenuItem("Help");
+            MenuItem PSItem = new MenuItem("Screenshot");
+            MenuItem QuitItem = new MenuItem("Quit");
+
+            PSNameItem.Enabled = false;
+
+            QuitItem.Click += quit;
+            PSItem.Click += PrintScreen;
+            HelpItem.Click += help;
+
+            ContextMenu contextMenu = new ContextMenu();
+            contextMenu.MenuItems.Add(PSNameItem);
+            contextMenu.MenuItems.Add("-");
+            contextMenu.MenuItems.Add(HelpItem);
+            contextMenu.MenuItems.Add(PSItem);
+            contextMenu.MenuItems.Add("-");
+            contextMenu.MenuItems.Add(QuitItem);
+
+            nIco.ContextMenu = contextMenu;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# PrintScreen
+
+## why?
+
+Windows already has this functionality (Winkey+PrintScreen). Why re-invent the wheel?
+
+Well, that is a keyboard shortcut. When you don't have a keyboard attached to the computer (ie, phone, tablet), there is no easy way to print screen! (that I know of, please let me know if I'm wrong)
+
+This project aims to fix that. It adds a small icon in the system tray that allows you to click to save a screenshot in the folder where other screenshots /should/ be saved (My Pictures\Screenshots).
+
+It's also second-safe, so if you want to save screenshots more than once per second, it's okay too (there's a 1/90 chance of a file getting duplicated if you take two screenshots within a second).
+
+## download
+
+download the file in `bin/Debug/PS.exe`.
